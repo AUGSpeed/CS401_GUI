26fd70a9557a881886c100c0782bbe136a78b49c
sahejhundal <hundalsahej@gmail.com>
2021-03-17 14:05:25 -0700

    Made Address and Name class

--- src/address/data/Address.java
+++ src/address/data/Address.java
@@ -1,4 +1,29 @@
 package address.data;
 
 public class Address {
+    String street;
+    String city;
+    String state;
+    int zip;
+
+    Address(){
+        street="NONE";
+        city="NONE";
+        state="NONE";
+        zip=0;
+    }
+    Address(String stree, String cit, String stat, int zi){
+        street=stree;
+        city=cit;
+        state=stat;
+        zip=zi;
+    }
+    String getStreet(){ return street; }
+    void setStreet(String x){ street = x; }
+    String getCity(){ return city; }
+    void setCity(String x){ city = x; }
+    String getState(){ return state; }
+    void setState(String x){ state = x; }
+    int getZip(){ return zip; }
+    void setZip(int x){ zip = x; }
 }
--- src/address/data/Name.java
+++ src/address/data/Name.java
@@ -1,4 +1,19 @@
 package address.data;
 
 public class Name {
+    String firstName;
+    String lastName;
+    Name(){
+        firstName="NONE";
+        lastName="NONE";
+    }
+    Name(String a, String b){
+        firstName=a;
+        lastName=b;
+    }
+
+    String getFirstName(){ return firstName; }
+    void setFirstName(String x){ firstName = x; }
+    String getLastName(){ return lastName; }
+    void setLastName(String x){ lastName = x; }
 }
